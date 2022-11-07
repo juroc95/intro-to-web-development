94e109883cc91b92b828c141efe598da72e12325
Juhwan <ljh1230vvv@gmail.com>
2022-11-06 23:37:05 -0800

    hw4 03 form

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -1,1 +1,33 @@
 // Add your code here
+const username_input = document.getElementById("username");
+const email_input = document.getElementById("email");
+const password_input = document.getElementById("password");
+const radio_yes = document.getElementById("yes");
+const radio_no = document.getElementById("no");
+const date_input = document.getElementById("date");
+
+document.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (
+    username_input.value === "" ||
+    email_input.value === "" ||
+    date_input.value === "" ||
+    password_input.value === ""
+  ) {
+    console.warn("You must enter some data to submit this form");
+    return;
+  }
+  if (!radio_yes.checked && !radio_no.checked) {
+    console.warn("You must enter some data to submit this form");
+    return;
+  }
+  console.log("========= Form Submission =========");
+  console.log("Username: " + username_input.value);
+  console.log("Email: " + email_input.value);
+  if (radio_yes.checked) {
+    console.log("Newsletter: " + radio_yes.value);
+  } else if (radio_no.checked) {
+    console.log("Newsletter: " + radio_no.value);
+  }
+  console.log("Date: " + date_input.value);
+});
